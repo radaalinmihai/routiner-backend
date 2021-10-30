a6450b8476402b33446f23c90ddf7e0e5c1d0cce
Alin Rada <radaalinmihai@gmail.com>
2021-10-31 00:38:40 +0300

    pretty

--- app/controllers/User/user.controller.ts
+++ app/controllers/User/user.controller.ts
@@ -1,43 +1,43 @@
 import {
-	ContextConfigDefault,
-	RawReplyDefaultExpression,
-	RawRequestDefaultExpression,
-	RawServerBase,
-	RouteHandlerMethod,
+  ContextConfigDefault,
+  RawReplyDefaultExpression,
+  RawRequestDefaultExpression,
+  RawServerBase,
+  RouteHandlerMethod,
 } from "fastify";
 import { UserBody, UserReturn } from "../../models/user.model";
 
 export const loginHandler: RouteHandlerMethod<
-	RawServerBase,
-	RawRequestDefaultExpression,
-	RawReplyDefaultExpression,
-	{ Body: UserBody; Reply: UserReturn },
-	ContextConfigDefault
+  RawServerBase,
+  RawRequestDefaultExpression,
+  RawReplyDefaultExpression,
+  { Body: UserBody; Reply: UserReturn },
+  ContextConfigDefault
 > = (req, reply) => {
-	const { server, body } = req;
-	const { username, email, password } = body;
-	server.mysql
-		.query("SELECT * FROM users WHERE email=?", [email])
-		.then((res) => {
-			const [rows, fields] = res.entries();
-			server.log.info("fields", rows);
-			return reply.send({
-				access_token: "access_token",
-			});
-		})
-		.catch((err) => {
-			server.log.error(err);
-		});
+  const { server, body } = req;
+  const { username, email, password } = body;
+  server.mysql
+    .query("SELECT * FROM users WHERE email=?", [email])
+    .then((res) => {
+      const [rows, fields] = res.entries();
+      server.log.info("fields", rows);
+      return reply.send({
+        access_token: "some access token that needs to be added",
+      });
+    })
+    .catch((err) => {
+      server.log.error(err);
+    });
 };
 
 export const registerHandler: RouteHandlerMethod<
-	RawServerBase,
-	RawRequestDefaultExpression,
-	RawReplyDefaultExpression,
-	{ Body: UserBody; Reply: UserReturn },
-	ContextConfigDefault
+  RawServerBase,
+  RawRequestDefaultExpression,
+  RawReplyDefaultExpression,
+  { Body: UserBody; Reply: UserReturn },
+  ContextConfigDefault
 > = (req, reply) => {
-	const { server, body } = req;
-	const { username, email, password } = body;
-	server.mysql;
+  const { server, body } = req;
+  const { username, email, password } = body;
+  server.mysql;
 };
