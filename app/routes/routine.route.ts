7e39fabebeb1468d1a5eac79c65f9a2d0353db22
Alin Rada <radaalinmihai@gmail.com>
2024-02-11 02:07:46 +0200

    changed husky script to run lint instead of lint:fix to not have files that have been formatted by eslint not go into the commit

--- app/routes/routine.route.ts
+++ app/routes/routine.route.ts
@@ -1,7 +1,15 @@
 import { FastifyInstance } from "fastify";
 import authenticationMiddleware from "../middlewares/authentication.js";
-import { deleteRoutine, getRoutine, insertRoutineHandler } from "../controllers/Routine/routine.controller.js";
-import { deleteRouteOptions, getRouteOptions, insertRoutineOptions } from "../schemas/routine.schema.js";
+import {
+	deleteRoutine,
+	getRoutine,
+	insertRoutineHandler,
+} from "../controllers/Routine/routine.controller.js";
+import {
+	deleteRouteOptions,
+	getRouteOptions,
+	insertRoutineOptions,
+} from "../schemas/routine.schema.js";
 
 export default async function routineRoutes(fastify: FastifyInstance) {
 	fastify.addHook("onRequest", authenticationMiddleware(fastify));
